c2f4b0ff53148b4f522700bdde00db5eddc60f47
Val <60014136+voffee@users.noreply.github.com>
2024-11-06 11:31:53 +0800

    Minor img CSS + modal show JS.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 
 // Define UI Variables
 const dialog = document.querySelector('.modal');
-const 
+const openModal = document.querySelector('#openModal');
+const closeModal = document.querySelector('#closeModal');
 
 // Add Event Listeners
+openModal.addEventListener('click', modalDisplay)
 
 // Create Functions
+function modalDisplay() {
+    dialog.showModal();
+}
